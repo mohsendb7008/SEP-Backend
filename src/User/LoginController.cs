91ea17b536b38ff7d65cfe457e61c0312665ab2f
mohsendb7008 <mohsendb7008@gmail.com>
2025-10-14 20:55:02 +0200

    user login controller and auth

---
+++ src/User/LoginController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SEP_Backend.User;
+
+[ApiController]
+public class LoginController(IUserRepository repository, JwtTokenGenerator jwtTokenGenerator) : Controller
+{
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromQuery] Guid userId)
+    {
+        var user = await repository.GetByIdAsync(userId);
+        if (user == null)
+            return Unauthorized();
+        var token = jwtTokenGenerator.Generate(userId);
+        return Ok(new { token });
+    } 
+}
--- src/User/UserController.cs
+++ src/User/UserController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SEP_Backend.User;
@@ -9,11 +10,14 @@ public class UserController(IUserRepository repository) : Controller
     public async Task<List<User>> GetAllAsync() => await repository.GetAllAsync();
 
     [HttpPost("users/create")]
+    [Authorize]
     public async Task<bool> CreateAsync([FromBody] User user) => await repository.CreateAsync(user);
 
     [HttpPut("users/update")]
+    [Authorize]
     public async Task<bool> UpdateAsync([FromBody] User user) => await repository.UpdateAsync(user);
 
     [HttpDelete("users/delete")]
+    [Authorize]
     public async Task<bool> DeleteAsync([FromQuery] Guid userId) => await repository.DeleteAsync(userId);
 }
