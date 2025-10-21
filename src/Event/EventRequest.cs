0335a2ca09f67b7db345a8994695418ff7d35b36
mohsendb7008 <mohsendb7008@gmail.com>
2025-10-21 22:53:07 +0200

    added event request

---
+++ src/Event/EventRequest.cs
@@ -0,0 +1,8 @@
+namespace SEP_Backend.Event;
+
+public class EventRequest
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public decimal Budget { get; set; }
+}
