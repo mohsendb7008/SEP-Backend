66bf96256042ed175bef6313f0bb95e0271e276f
mohsendb7008 <mohsendb7008@gmail.com>
2025-10-25 19:17:10 +0200

    added get by id for event controller

--- src/Event/EventController.cs
+++ src/Event/EventController.cs
@@ -10,11 +10,19 @@ public class EventController(IEventRepository repository, EventService service) 
     [Authorize]
     public async Task<List<Event>> GetAllAsync() => await repository.GetAllAsync();
 
+    [HttpGet("events/{eventId:guid}")]
+    [Authorize]
+    public async Task<Event?> GetByIdAsync([FromRoute] Guid eventId) => await repository.GetByIdAsync(eventId);
+
     [HttpPost("events/create")]
     [Authorize(Roles = "CustomerServiceOfficer")]
     public async Task CreateAsync([FromBody] EventRequest request) => await service.CreateAsync(request);
 
-    [HttpPut("events/update")]
+    [HttpPut("events/{eventId:guid}")]
     [Authorize(Policy = "Admin")]
-    public async Task<bool> UpdateAsync([FromBody] Event @event) => await repository.UpdateAsync(@event);
+    public async Task<bool> UpdateAsync([FromRoute] Guid eventId, [FromBody] Event @event)
+    {
+        @event.Id = eventId;
+        return await repository.UpdateAsync(@event);
+    }
 }
